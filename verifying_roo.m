6c083c666d138aad2df3562c09e6d40e6b56d99e
krutledg <krutledg@xuxiangr.eecs.umich.edu>
2017-09-28 16:07:02 -0400

    Prepping for new files related to new model and improved tests.

--- verifying_roo.m
+++ verifying_roo.m
@@ -7,49 +7,6 @@ clc;
 
 %% Constants
 
-% ki = 0.2890; % already divided by m
-% f_0 = 0.6558; % already divided by m
-% f_1 = 0.0058; % already divided by m
-% T = 1/25; % controller period
-
-% %% Continuous matrices
-% A = [-f_1, 0, 0;
-%      -1, 0, 1;
-%       0, 0, 0;];
-% B = [ki;
-%      0;
-%      0;];
-% F = [-f_0;
-%       0;
-%       0;];
-% E = [0;
-%      0;
-%      1;];
-% C = [1, 0, 0;
-%      0, 1, 0];
-
-% %% convert to discret time
-% A_s = @(s) expm(s*A);
-% Ad = A_s(T);
-% Bd = integral(A_s, 0, T, 'ArrayValued', true) * B;
-% Fd = integral(A_s, 0, T, 'ArrayValued', true) * F;
-% Ed = integral(A_s, 0, T, 'ArrayValued', true) * E;
-
-% % Ad = [Aaa, Aau;
-% %       Aua, Auu];
-% Aaa = Ad(1:2,1:2);
-% Aau = Ad(1:2,3);
-% Aua = Ad(3,1:2);
-% Auu = Ad(3,3);
-
-% Ba = Bd(1:2,1);
-% Bu = Bd(3,1);
-% Fa = Fd(1:2,1);
-% Fu = Fd(3,1);
-
-% Ea = Ed(1:2,1);
-% Eu = Ed(3,1);
-
 addpath('./OzayTest1/')
 
 con = constants_normal; 

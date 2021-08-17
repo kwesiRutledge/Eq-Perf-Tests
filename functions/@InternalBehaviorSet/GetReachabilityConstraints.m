142fb5149f037e7bdcec7dbfad3040e1cc6c6851
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2021-08-16 21:49:59 -0400

    Updated to reduce some of the extra lines, but still need to consider controlling the system through the multiple words that may be in effect in the final time step.

--- functions/@InternalBehaviorSet/GetReachabilityConstraints.m
+++ functions/@InternalBehaviorSet/GetReachabilityConstraints.m
@@ -27,6 +27,8 @@ function [ constraints , dual_vars ] = GetReachabilityConstraints( varargin )
 
 	cg = constr_gen(0);
 
+	x0 = System.X0.V';
+
 	%%%%%%%%%%%%%%%
 	%% Algorithm %%
 	%%%%%%%%%%%%%%%
@@ -54,7 +56,7 @@ function [ constraints , dual_vars ] = GetReachabilityConstraints( varargin )
 	h = [ b ; be ; -be ; W_final.b];
 
 	if ~isempty(W_final.Ae)
-		H = [ H ; zeros(size(W_final.Ae,1),ibs.Dim) , W_final.Ae ; zeros(size(W_final.Ae,1),ibs.Dim) , -W_final.Ae];
+		H = [ H ; zeros(size(W_final.Ae,1),ibs.Dim) , W_final.Ae ; zeros(size(W_final.Ae,1),ibs.Dim) , -W_final.Ae ];
 		h = [ h ; W_final.be ; -W_final.be ];
 	end
 
@@ -65,25 +67,40 @@ function [ constraints , dual_vars ] = GetReachabilityConstraints( varargin )
 	selectW1 = SelectWMatrices{1};
 	selectX0 = SelectX0Matrices{1};
 
-	selectWAndX0 = [ 	selectW1, zeros(n_w*(T-1),n_w) ; 
-						[ zeros(n_w,ibs.Dim) , eye(n_w) ]  ;  
-						selectX0 , zeros(n_x,n_w) ];
+	% selectWAndX0 = [ 	selectW1, zeros(n_w*(T-1),n_w) ; 
+	% 					[ zeros(n_w,ibs.Dim) , eye(n_w) ]  ;  
+	% 					selectX0 , zeros(n_x,n_w) ];
+
+	% %% Creating Constraints %%
+
+	% % Create Target Matrices
+	% [ S_w , S_u , S_C , S_x0 , S_k , S_Bw , S_Cv ] = System.get_mpc_matrices('word',word1); %Get MPC Matrices
+	% G = [ S_w*S_Bw + S_u*ibs.K , S_x0 ];
+
+	% selectFinalState = [ zeros(n_x,n_x*T) , eye(n_x) ];
+
+	% H_T = X_Target.A * selectFinalState * G * selectWAndX0;
+	% h_T = X_Target.b - X_Target.A * selectFinalState * ( S_w * S_k + S_u * ibs.k);
+
+	selectWAll = [ 	selectW1, zeros(n_w*(T-1),n_w) ; 
+					[ zeros(n_w,ibs.Dim) , eye(n_w) ] ];
+
+	%% Creating Constraints %%
 
 	% Create Target Matrices
-	[S_w,S_u,S_C,S_x0,S_k,S_Bw,S_Cv] = System.get_mpc_matrices('word',word1); %Get MPC Matrices
-	G = [ 	S_w*S_Bw+S_u*ibs.K , ...
-			S_x0 ];
+	[ S_w , S_u , S_C , S_x0 , S_k , S_Bw , S_Cv ] = System.get_mpc_matrices('word',word1); %Get MPC Matrices
+	G = [ S_w*S_Bw + S_u*ibs.K ];
 
 	selectFinalState = [ zeros(n_x,n_x*T) , eye(n_x) ];
 
-	H_T = X_Target.A * selectFinalState * G * selectWAndX0;
-	h_T = X_Target.b - X_Target.A * selectFinalState * ( S_w * S_k + S_u * ibs.k);
+	H_T = X_Target.A * selectFinalState * G * selectWAll;
+	h_T = X_Target.b - X_Target.A * selectFinalState * ( S_x0*x0 + S_w * S_k + S_u * ibs.k);
 
 	% [ dual_vars , constraints ] = cg.create_sadraddini_AH_inclusion_constr( ...
 	% 	zeros(n_w*T+n_x,1) , selectWAndX0   , H , h , ...
 	% 	zeros(n_w*T+n_x,1) , [eye(n_w*T+n_x)] , H_T , h_T );
 
-	[ dual_vars , constraints ] = cg.get_H_polyt_inclusion_constr( H , h , H_T, h_T );
+	[ dual_vars , constraints ] = cg.get_H_polyt_inclusion_constr( H , h , H_T , h_T );
 
 
 end

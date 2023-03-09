96a67e5af02a3396bc58703b00cb0e9185c04d55
Kwesi Rutledge <kwesir@mit.edu>
2023-03-09 16:53:35 -0500

    Created new figure (colors and input constraints) for the hidden info task.

--- results/acc2023/hide_info3.m
+++ results/acc2023/hide_info3.m
@@ -1,7 +1,11 @@
-%hide_info2.m
+%hide_info3.m
 %Description:
 %	This test will attempt to synthesize a controller that satisfies the hidden mode specification.
-%	It doesn't use the drone model, but instead a simple scalar system.
+%	This example uses a simple 2d drone model with a wacky B matrix.
+%   While one input needs to remain "0" to keep th emode of the system
+%   "hidden" from outside observers, the other input needs to aggressively
+%   act to satisfy a task.
+%
 %	The task:
 %
 %		/^\_{i = 1,2} <> K { \mode_i \} \implies <> X_i
@@ -15,7 +19,11 @@ addpath(genpath('../../functions/'))
 
 %% Constants %%
 
-[ lcsas0 , x0 , TimeHorizon , P_target ] = get_scalar_lcsas_for_hiding2d('eta_u',0.1);
+rng("default")
+
+[ lcsas0 , x0 , TimeHorizon , P_target ] = get_scalar_lcsas_for_hiding2d( ...
+    'eta_u',2.0, ...
+    'W',Polyhedron('lb',[0.8,-0.1],'ub',[1.2,0.1]));
 
 % P_Targets = [ P_target , P_target - [0.7;0] ];
 
@@ -45,14 +53,31 @@ if strcmp(info.Message,'Solved!')
 % 	plot(P_targets(2),'color','magenta','alpha',0.5)
     ic_plot = scatter(lcsas0.X0.V(1),lcsas0.X0.V(2),x0_markersize,'black','filled');
 
+    mode1_count = 0;
+    mode2_count = 0;
 	for simulation_index = 1:10
 		[ x_0_t, u_0_tm1 , y_0_t , sig ] = scalar_system_controller.simulate_1run();
 
-		% for t = 0:TimeHorizon
-		% 	x_t = x_0_t(:,t+1);
-		% 	scatter(x_t(1),x_t(2))
-		% end
-		plot(x_0_t(1,:),x_0_t(2,:),'LineWidth',trajectory_lw)
+		if sig(1) == 1
+            %trajectory_color = 'yellow';
+            % trajectory_color = "#6200EE";
+            trajectory_color = [98,0,238]/255;
+            trajectory_color(3) = trajectory_color(3) - (40/255) * mode1_count;
+            mode1_count = mode1_count + 1;
+        elseif sig(1) == 2
+            %trajectory_color = 'blue';
+            %trajectory_color = "#03DAC6";
+            trajectory_color = [3, 218, 198]/255;
+            trajectory_color(2) = trajectory_color(2) - (30/255)*mode2_count;
+
+            mode2_count = mode2_count + 1;
+        else
+            error([ 'Unexpected sig value! ' num2str(sig) ])
+        end
+
+		plot(x_0_t(1,:),x_0_t(2,:), ...
+            'LineWidth',trajectory_lw, ...
+            'Color', trajectory_color)
 		
 
 		%Save data
@@ -60,7 +85,7 @@ if strcmp(info.Message,'Solved!')
 
 	end
 
-	axis([-0.5,2.5,-0.5,3.0])
+	axis([-0.5,3.0,-1.0,3.0]) 
     xticks([0 1 2])
 
     xlabel('$p^{(x)}$','Interpreter','latex','FontSize',axis_fs)
